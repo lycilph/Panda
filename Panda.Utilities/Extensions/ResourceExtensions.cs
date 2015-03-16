02e8f4653d57b828a92de70d5f97521b1d1fa24a
lycilph <lycilph@gmail.com>
2015-03-16 11:45:28 +0100

    Updated nuget packages and added the ResourceExtensions

---
+++ Panda.Utilities/Extensions/ResourceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Panda.Utilities.Extensions
+{
+    public static class ResourceExtensions
+    {
+        public static string GetResource(string name)
+        {
+            var resource_name = Assembly.GetExecutingAssembly().GetManifestResourceNames().First(n => n.Contains(name));
+            using (var s = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource_name))
+            using (var sr = new StreamReader(s))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
